0f9c5a30ddfc22903d1153360a6990d7e690ac94
Dhymas Gabriel <dhymas.g@gmail.com>
2023-06-21 15:30:33 -0300

    teste de export e import

---
+++ modulos/md001/export001.js
@@ -0,0 +1,4 @@
+const cores=["Azul","Verde","Roxo","Preto"]
+const numeros=["Um","Dois","Tres","Quatro"]
+
+export {cores,numeros}
---
+++ modulos/md001/md001.js
@@ -0,0 +1,6 @@
+import { cores } from "./export001.js"
+import { numeros } from "./export001.js"
+console.clear()
+console.log("servidor rodando")
+console.log(cores)
+console.log(numeros)
