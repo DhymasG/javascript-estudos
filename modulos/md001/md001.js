c2b7325266d0cf3cc0333778a5105b8a4ca1e5f7
Dhymas Gabriel <dhymas.g@gmail.com>
2023-06-21 15:59:07 -0300

    usando modulos renomeados

--- modulos/md001/md001.js
+++ modulos/md001/md001.js
@@ -1,11 +1,15 @@
 import getTodasCores from "./export001.js"
-import { cores } from "./export001.js"
-import { numeros } from "./export001.js"
+// import { cores } from "./export001.js"
+// import { numeros } from "./export001.js"
+import * as m_cores from "./export001.js"
 
 
 
 console.clear()
-console.log("servidor rodando")
-console.log(cores)
-console.log(numeros)
-console.log(getTodasCores())
+// console.log("servidor rodando")
+// console.log(cores)
+// console.log(numeros)
+// console.log(getTodasCores())
+console.log(m_cores.cores)
+console.log(m_cores.numeros)
+console.log(m_cores.default)
