49b0baff6c62a7a4a49175dc4c06f7aca8cdb1c5
Dhymas Gabriel <dhymas.g@gmail.com>
2023-06-21 15:51:12 -0300

    usando export default

--- modulos/md001/export001.js
+++ modulos/md001/export001.js
@@ -1,4 +1,10 @@
 const cores=["Azul","Verde","Roxo","Preto"]
 const numeros=["Um","Dois","Tres","Quatro"]
 
+
+
+export default function getTodasCores(){
+    return cores
+}
+
 export {cores,numeros}
--- modulos/md001/md001.js
+++ modulos/md001/md001.js
@@ -1,6 +1,11 @@
+import getTodasCores from "./export001.js"
 import { cores } from "./export001.js"
 import { numeros } from "./export001.js"
+
+
+
 console.clear()
 console.log("servidor rodando")
 console.log(cores)
 console.log(numeros)
+console.log(getTodasCores())
